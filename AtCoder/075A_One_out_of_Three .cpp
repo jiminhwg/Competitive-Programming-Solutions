54e1bf5f91dacb5cba3fda8ad3ebfdaeb6ac43d4
Jimin Hwang <84829334+sunfan05@users.noreply.github.com>
2024-05-21 11:02:01 +0900

    Add files via upload

---
+++ AtCoder/075A_One_out_of_Three .cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+  int A, B, C;
+  cin >> A >> B >> C;
+  if (A!=B and A!= C) {
+      cout << A << endl;
+  }
+  else if (B != A and B != C) {
+      cout << B << endl;
+  }
+  else {
+      cout << C << endl;
+  }
+}
---
+++ AtCoder/096A_Day_of_Takahashi.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+    int A, B;
+    cin >> A >> B;
+    if (A <= B) {
+        cout << A << endl;
+    }
+    else {
+        cout << A-1 << endl;
+    }
+}
